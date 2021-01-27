20f5734a78f5c28377d9c5d2534b49d9d324457d
Conor Walsh <116340836@umail.ucc.ie>
2021-01-27 16:03:53 +0000

    Fixed memory issue

--- apps/ni_daq_test.cpp
+++ apps/ni_daq_test.cpp
@@ -3,10 +3,7 @@
 #include <stdio.h>
 #include <NIDAQmx.h>
 
-
-//#include <Eigen/Dense>
 #include <Dense>
-
 
 
 using Eigen::MatrixXd;
@@ -23,6 +20,7 @@ MatrixXf    buffer_result;
 
 int32 CVICALLBACK EveryNCallback(TaskHandle taskHandle, int32 everyNsamplesEventType, uInt32 nSamples, void* callbackData);
 int32 CVICALLBACK DoneCallback(TaskHandle taskHandle, int32 status, void* callbackData);
+
 int read_data_buffer(int num_of_channels_used, int samps_per_chan);
 char* construct_channel_name(string channel_num);
 
@@ -47,45 +45,6 @@ int main() {
 	char        errBuff[2048] = { '\0' };
 
 
-	/*
-
-
-	// Get the User configurable parameters
-
-	cout << "Enter the number of channels you want to use : " << endl;
-	cin >> num_channels;
-
-	cout << "Enter the analog input channel used (1-15) : " << endl;
-	cin >> channel;
-	channel = "Dev1/ai" + channel;
-
-	char* channel_char_arr;
-	string str_obj(channel);
-	channel_char_arr = &str_obj[0];
-	//cout << channel_char_arr << endl;
-
-	cout << "Enter a value for the minimum voltage (V) : " << endl;
-	cin >> min_voltage;
-
-	cout << "Enter a value for the maximum voltage (V) : " << endl;
-	cin >> max_voltage;
-
-	cout << "Enter a sample rate of the DAQ (Hz) : " << endl;
-	cin >> sample_rate;
-	//sample_rate = sample_rate * 1000;
-
-	cout << "Enter the number of samples per channel : " << endl;
-	cin >> samples_per_chan;
-	//user_samp_per_chan = sample_rate;
-
-	array_size = samples_per_chan * num_channels;
-
-	cout << "Enter the number of samples : " << endl;
-	cin >> n_samples;
-
-
-	*/
-
 	// Construct the string to specify the channel used and then write as char array
 
 	string ai_channel = "1";
@@ -96,36 +55,39 @@ int main() {
 
 
 
-	//channel_as_char_arr = construct_channel_name("1");
+	//channel_as_char_arr = construct_channel_name("4");           // Create the channel character array
 	//cout << "Channel : " << channel_as_char_arr << endl;
 	//getchar();
 
+
+
 	// User configurable parameters
 
-	min_voltage = 0;    // Voltage swing should be between -10 V -> +10 V
+	min_voltage = 0;           // Voltage swing should be between -10 V -> +10 V
 	max_voltage = 5;
 
-	sample_rate = 5000;        // Set the sample rate of the DAQ for all channels in Hz
-							  // Regardless if they are read or not this is how often the DAQ looks at the channel inputs.
+	sample_rate = 10000;       // Set the sample rate of the DAQ for all channels in Hz
+							   // Regardless if they are read or not this is how often the DAQ looks at the channel inputs.
 
-	n_samples = 1000;           // After this many samples are gathered the EveryNSamplesEvent will be called.
-							   // This calls the function EveryNCallback which calls the DAQmxReadAnalogF64 function
+	n_samples = 500;           // After this many samples are gathered PER CHANNEL the EveryNSamplesEvent will be called.
+							   // This event calls the function EveryNCallback (which calls the DAQmxReadAnalogF64 function)
 
-	samples_per_chan = 500;    // Specify the number of samples read by each channel
+	samples_per_chan = 500;    // Specify the number of samples read by each channel (usually ignored)
+							   // This is used to calculate the buffer size if the default isnt appropriate
 
-	num_channels = 2;          // Setting the number of channels used to determine buffer size 
+	num_channels = 2;          // Set the number of channels used to determine buffer size. 
 
-	array_size = samples_per_chan * num_channels;   // The array to read samples into, organized according to fillMode.
+	array_size = n_samples * num_channels;   // The array to read samples into, organized according to fillMode.
 
-	buffer_result.resize(samples_per_chan, num_channels);  // Creating an Eigen Matrix that reads and stores the data buffer
+	buffer_result.resize(samples_per_chan, num_channels);    // Creating an Eigen Matrix that reads and stores the data buffer
 												             // Each column represents the samples for a particular channel
 
 	int a = 1;
 	string exit;
 
-	/*********************************************/
-		// DAQmx Configure Code
-	/*********************************************/
+	
+		//******** DAQmx Configure Code ********\\
+
 	DAQmxErrChk(DAQmxCreateTask("", &taskHandle));
 
 	//DAQmxErrChk (DAQmxCreateAIVoltageChan(taskHandle,"Dev1/ai0","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,NULL));
@@ -137,42 +99,23 @@ int main() {
 	//DAQmxErrChk(DAQmxCfgSampClkTiming(taskHandle, "", 100, DAQmx_Val_Rising, DAQmx_Val_ContSamps, 10));
 	DAQmxErrChk(DAQmxCfgSampClkTiming(taskHandle, "", sample_rate, DAQmx_Val_Rising, DAQmx_Val_ContSamps, samples_per_chan));
 
-
 	//DAQmxErrChk(DAQmxRegisterEveryNSamplesEvent(taskHandle, DAQmx_Val_Acquired_Into_Buffer, 10, 0, EveryNCallback, NULL));
 	DAQmxErrChk(DAQmxRegisterEveryNSamplesEvent(taskHandle, DAQmx_Val_Acquired_Into_Buffer, n_samples, 0, EveryNCallback, NULL));
 
-
 	DAQmxErrChk(DAQmxRegisterDoneEvent(taskHandle, 0, DoneCallback, NULL));
 
-	/*********************************************/
-	// DAQmx Start Code
-	/*********************************************/
 
+
+	
+	//******** DAQmx Start Code ********\\
+	
 	DAQmxErrChk(DAQmxStartTask(taskHandle));
 
+
+
 	printf("Acquiring samples continuously. Press Enter to interrupt\n");
-	getchar();
-
 	
-
-
-
-
-	/*
-	
-	while (exit != "q")
-	{
-		exit = getchar();
-
-		if (exit == "b")
-			read_data_buffer();
-
-	}
-	
-	*/
-
-
-
+	getchar();   // Wait for a key press to stop the code
 	
 
 
@@ -180,9 +123,9 @@ Error:
 	if (DAQmxFailed(error))
 		DAQmxGetExtendedErrorInfo(errBuff, 2048);
 	if (taskHandle != 0) {
-		/*********************************************/
-		// DAQmx Stop Code
-		/*********************************************/
+		
+		//******** DAQmx Stop Code ********\\
+		
 		DAQmxStopTask(taskHandle);
 		DAQmxClearTask(taskHandle);
 	}
@@ -201,9 +144,9 @@ int32 CVICALLBACK EveryNCallback(TaskHandle taskHandle, int32 everyNsamplesEvent
 	int32       read = 0;
 	float64     data[1000];
 
-	/*********************************************/
-	// DAQmx Read Code
-	/*********************************************/
+	
+	//******** DAQmx Read Code ********\\
+	
 
 	//DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, samples_per_chan, 10.0, DAQmx_Val_GroupByScanNumber, data, array_size, &read, NULL));
 
@@ -213,17 +156,16 @@ int32 CVICALLBACK EveryNCallback(TaskHandle taskHandle, int32 everyNsamplesEvent
 	//if (read > 0) {   }
 
 	
-	// Call the function that transfers the buffer data into an eigen matrix
-	// with each column representing a channel
+	// Call the function that transfers the buffer data into an eigen matrix with each column representing a channel
 	read_data_buffer(num_channels, samples_per_chan);
 
 
 Error:
 	if (DAQmxFailed(error)) {
 		DAQmxGetExtendedErrorInfo(errBuff, 2048);
-		/*********************************************/
+		
 		// DAQmx Stop Code
-		/*********************************************/
+		
 		DAQmxStopTask(taskHandle);
 		DAQmxClearTask(taskHandle);
 		printf("DAQmx Error: %s\n", errBuff);
@@ -250,17 +192,15 @@ Error:
 
 int read_data_buffer(int num_of_channels_used, int samps_per_chan) {
 
-	//cout << "You asked to read the buffer!" << endl;
-
-	//printf("Data point 5 has value %f\n", buff_data[5]);
+	// Sorts the buffer into columns in an eigen matrix
 
 	for (int j = 0; j < num_of_channels_used; j++)	
 		for (int i = 0; i < samps_per_chan; i++)
-			buffer_result(i , j) = buff_data[i + (j * samps_per_chan)];
+			buffer_result(i , j) = buff_data[i + (j * samps_per_chan)];      
 		
 
-	cout << "First result in column 1 = " << buffer_result(1, 0) << "\t" <<    // Print the fifth result
-	  "First result in column 2 = " << buffer_result(1, 1) << endl;    // Print the fifth result
+	cout << "First result in column 1 = " << buffer_result(0, 0) << "\t" <<    // Print the fifth result
+	  "First result in column 2 = " << buffer_result(0, 1) << endl;    // Print the fifth result
 
 	
 
